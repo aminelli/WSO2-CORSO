15c7658758cb14b8c926559dc9f17cf0544de6d7
aminelli <antonio.minelli@etlforma.com>
2025-05-27 11:00:16 +0200

    upd

--- docker/01-postgres/batch-02-pgadmin.bat
+++ docker/01-postgres/batch-02-pgadmin.bat
@@ -6,7 +6,7 @@ docker network create net-wso2
 
 echo "== CREAZIONE CONTAINER POSTGRESQL =="
 
-docker run -d --name pgadmin-wso2 --hostname pgadmin-wso2 -e PGADMIN_DEFAULT_EMAIL=admin@wso2.com -e PGADMIN_DEFAULT_PASSWORD=admin --network $networkName -p 5050:80 -v vol_wso2_pgadmin:/var/lib/pgadmin dpage/pgadmin4:latest
+docker run -d --name pgadmin-wso2 --hostname pgadmin-wso2 -e PGADMIN_DEFAULT_EMAIL=admin@wso2.com -e PGADMIN_DEFAULT_PASSWORD=admin --network net-wso2 -p 5050:80 -v vol_wso2_pgadmin:/var/lib/pgadmin dpage/pgadmin4:latest
 
 
 echo "###### AVVIO CONTAINER TERMINATO ######"
